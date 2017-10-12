205ece0805b47aeb259274026322ea2238d070d8
Wisse Voortman <w_voortman@hotmail.com>
2017-10-12 19:09:21 +0200

    made some changes to the view class so that it can dynamicaly create objects of the arduinoGUI class based on the COM ports that have an arduino connected
    problem: how to add gui elements to root defined in View class from another class

--- python/MainView.py
+++ python/MainView.py
@@ -9,52 +9,55 @@ class View:
         """Initialize the main Program View.
         """
         self.ports = list(serial.tools.list_ports.comports())
-        root = Tk() # initialize Root window
 
-        root.title('Centrale Project 2.1 - Computer System ') # set title for Root window
+        self.root = Tk() # initialize Root window
 
-        root.geometry("800x500+200+200") # set size and location for Root window
+        self.root.title('Centrale Project 2.1 - Computer System ') # set title for Root window
 
-        mainframe = ttk.Frame(root, padding="3 3 12 12") # left top right bottem    create mainframe in Root winow
+        self.root.geometry("800x500+200+200") # set size and location for Root window
 
-        mainframe.grid(column=0, row=0, sticky=(N, W, S, E)) # set mainframe to root windows size
+        self.mainframe = ttk.Frame(self.root, padding="3 3 12 12") # left top right bottem    create mainframe in Root winow
 
-        menubar = Menu(mainframe) # create a menubar
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, S, E)) # set mainframe to root windows size
+
+        menubar = Menu(self.mainframe) # create a menubar
 
         # create a pulldown menu "FILE", and add it to the menu bar
         filemenu = Menu(menubar, tearoff=0)
         filemenu.add_command(label="Team", command=View.team)
         filemenu.add_separator()
-        filemenu.add_command(label="Exit", command=root.quit)
+        filemenu.add_command(label="Exit", command=self.root.quit)
         menubar.add_cascade(label="Commands", menu=filemenu) # se the label in the menubar
 
         # display the menu
-        root.config(menu=menubar)
+        self.root.config(menu=menubar)
 
-        mainframe.columnconfigure(0, weight=1)
-        mainframe.rowconfigure(0, weight=1)
+        self.mainframe.columnconfigure(0, weight=1)
+        self.mainframe.rowconfigure(0, weight=1)
 
-        label = Label(mainframe, text="hieronder de control units:")
+        label = Label(self.mainframe, text="hieronder de control units:")
         label.pack()
+
+        label2 = Label(self.mainframe, text="hier een 2e label")
+        label2.pack()
 
         # arduino detection
         View.checkPortsOnce()
 
         for p in View.ports:
             if "Arduino" in p[1]:
-
                 print("Arduino Gevonden op Poort: ", p[0])
-                exec("View." + p[0] + " = 'something else'") # going to assign a class here that is going to render the gui items that belong to a specific arduino
+                exec("View." + p[0] + " = arduinoGUI.GUI()") # going to assign a class here that is going to render the gui items that belong to a specific arduino
                 print("printing variable com3: ", View.COM3)
+
             else:
                 pass
 
 
 
-        root.mainloop()
+        self.root.mainloop()
 
     def checkPortsOnce():
-
         View.ports = list(serial.tools.list_ports.comports())
 
         for p in View.ports:
