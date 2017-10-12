14704317a96110b155af54583a8e0e3eb45aa267
Mitchel <mitchelvanrijn@hotmail.nl>
2017-10-13 00:00:46 +0200

    Added state indicator to Arduino class.

--- python/Arduino.py
+++ python/Arduino.py
@@ -8,20 +8,26 @@ import _thread
 class Arduino:
     _port = 0
     _stop = False  # Used to exit threads within THIS class.
+    _state = STATE.STOPPED
 
     def __init__(self, port):
         self._port = port
 
     def start(self):
         self._stop = False  # Make sure it's not going to stop immediately.
+        self._state = STATE.RUNNING
         # TODO: Add listener
 
     def stop(self):
         self._stop = True
+        self._state = STATE.STOPPED
         if DEBUG:
             print('Sending {0} signal to Arduino on port: {1}'
                   .format(color('STOP', COLORS.RED),
                           color(self._port, COLORS.CYAN)))
+
+    def get_state(self):
+        return self._state
 
     def get_port(self):
         return self._port
