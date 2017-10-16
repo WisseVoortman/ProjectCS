1f2906bd6c2cd2ea22d43aacaab6c06467499636
Wisse Voortman <w_voortman@hotmail.com>
2017-10-16 14:24:26 +0200

    Removed scrap code renamed some files

--- python/arduinoGUI.py
+++ python/arduinoGUI.py
@@ -9,11 +9,19 @@ class arduinoGUI:
         self.label1 = Label(self.frame, text="arduino GUI: ", bg='red')
         self.label1.grid(column=0, row=0)
 
-        self.button1 = ttk.Button(self.frame, text='New Window', width=25, command=self.team)
+        self.button1 = ttk.Button(self.frame, text='Geile button', width=25, command=self.team)
         self.button1.grid(column=0, row=1)
 
-        self.frame.pack()
+        # hier wordt begonnen aan de daadwerkelijke GUI above was een test
+        self.mode = Label(self.frame, text = "Huidige modus: ")
+        self.status = Label(self.frame, text = "Huidige status")
+        self.changeMode = ttk.Button(self.frame, text='change mode', width=25, command=self.team) # bij command kunnen functies worden aangeroepen  uit de controller of gewoon deze file
+        self.inrollen = ttk.Button(self.frame, text='Roll in', width=25, command=self.team)
+        self.uitrollen = ttk.Button(self.frame, text='Roll out', width=25, command=self.team)
 
+        self.frame.pack() # hier word de arudinoGUI aan de mainframe toegevoegd
+
+    # junkfunctie voor test
     def team():
         print("Team 6")
 
--- python/main.py
+++ python/main.py
@@ -1,6 +1,50 @@
+from tkinter import *
+from tkinter import ttk
 import serial.tools.list_ports
 import time
-import View2
+from arduinoGUI import *
 
-view2.main()
+
+class mainView:
+    def __init__(self, master):
+        """Initialize the main Program View.
+        """
+        self.ports = list(serial.tools.list_ports.comports())
+        self.master = master  # initialize Root window
+        self.master.title('Centrale Project 2.1 - Computer System ')  # set title for Root window
+        self.master.geometry("800x500+200+200")  # set size and location for Root window
+        self.mainframe = ttk.Frame(self.master, padding="0 0 0 0")  # left top right bottem    create mainframe in Root winow
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, S, E))  # set mainframe to root windows size
+
+        menubar = Menu(self.master)  # create a menubar
+
+        # create a pulldown menu "FILE", and add it to the menu bar
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Team", command=self.team)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=self.master.quit)
+        menubar.add_cascade(label="Commands", menu=filemenu)  # se the label in the menubar
+
+        # display the menu
+        self.master.config(menu=menubar)
+
+        label = Label(self.mainframe, text="Hieronder de control units: ", bg='red')
+        label.pack()
+
+        # for element in list of arduino's
+        arduinoGUI(self.mainframe)
+        arduinoGUI(self.mainframe)
+
+
+    def team():
+        print("Team 6")
+# END OF class: Mainview--------------------------------------------------------------------------
+
+
+def main():
+    root = Tk()
+    mainView(root)
+    root.mainloop()
+
+main()
 
