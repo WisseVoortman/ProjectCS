6cf4109550ec31c5fbe55a89141652414c20f356
Wisse Voortman <w_voortman@hotmail.com>
2017-10-16 15:05:15 +0200

    switched to ttk.notebook rather than frames for the arduino GUI part
    every arduino will get its own tab in the notebook

--- python/arduinoGUI.py
+++ python/arduinoGUI.py
@@ -2,25 +2,45 @@ from tkinter import *
 from tkinter import ttk
 
 class arduinoGUI:
-    def __init__(self, master):
+    def __init__(self, master, name):
+        self.name = name
         self.master = master
         self.frame = ttk.Frame(self.master)
 
-        self.label1 = Label(self.frame, text="arduino GUI: ", bg='red')
-        self.label1.grid(column=0, row=0)
-
-        self.button1 = ttk.Button(self.frame, text='Geile button', width=25, command=self.team)
-        self.button1.grid(column=0, row=1)
+        self.row = 0
 
         # hier wordt begonnen aan de daadwerkelijke GUI above was een test
+        self.id = Label(self.frame, text = "ID")
+        self.id.grid(column=0, row=self.row)
+
+        self.row = self.row + 1
+
         self.mode = Label(self.frame, text = "Huidige modus: ")
+        self.mode.grid(column=0, row=self.row)
+
+        self.row = self.row + 1
+
         self.status = Label(self.frame, text = "Huidige status")
-        self.changeMode = ttk.Button(self.frame, text='change mode', width=25, command=self.team) # bij command kunnen functies worden aangeroepen  uit de controller of gewoon deze file
-        self.inrollen = ttk.Button(self.frame, text='Roll in', width=25, command=self.team)
-        self.uitrollen = ttk.Button(self.frame, text='Roll out', width=25, command=self.team)
+        self.status.grid(column=0, row=self.row)
 
-        self.frame.pack() # hier word de arudinoGUI aan de mainframe toegevoegd
+        self.row = self.row + 1
 
+        self.changeMode = ttk.Button(self.frame, text='change mode', width=25, command=arduinoGUI.team) # bij command kunnen functies worden aangeroepen  uit de controller of gewoon deze file
+        self.changeMode.grid(column=0, row=self.row)
+
+        self.row = self.row + 1
+
+        self.inrollen = ttk.Button(self.frame, text='Roll in', width=25, command=arduinoGUI.team)
+        self.inrollen.grid(column=0, row=self.row)
+
+        self.row = self.row + 1
+
+        self.uitrollen = ttk.Button(self.frame, text='Roll out', width=25, command=arduinoGUI.team)
+        self.uitrollen.grid(column=0, row=self.row)
+
+        self.row = self.row + 1
+
+        self.master.add(self.frame, text=self.name) # deze moet nog even de text gewijzigd worden naar iets dynamics
     # junkfunctie voor test
     def team():
         print("Team 6")
--- python/main.py
+++ python/main.py
@@ -13,14 +13,14 @@ class mainView:
         self.master = master  # initialize Root window
         self.master.title('Centrale Project 2.1 - Computer System ')  # set title for Root window
         self.master.geometry("800x500+200+200")  # set size and location for Root window
-        self.mainframe = ttk.Frame(self.master, padding="0 0 0 0")  # left top right bottem    create mainframe in Root winow
+        self.mainframe = ttk.Notebook(self.master, padding="0 0 0 0")  # left top right bottem    create mainframe in Root winow
         self.mainframe.grid(column=0, row=0, sticky=(N, W, S, E))  # set mainframe to root windows size
 
         menubar = Menu(self.master)  # create a menubar
 
         # create a pulldown menu "FILE", and add it to the menu bar
         filemenu = Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Team", command=self.team)
+        filemenu.add_command(label="Team", command=mainView.team)
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=self.master.quit)
         menubar.add_cascade(label="Commands", menu=filemenu)  # se the label in the menubar
@@ -28,12 +28,12 @@ class mainView:
         # display the menu
         self.master.config(menu=menubar)
 
-        label = Label(self.mainframe, text="Hieronder de control units: ", bg='red')
-        label.pack()
+        #label = Label(self.mainframe, text="Hieronder de control units: ", bg='red')
+        #label.pack()
 
-        # for element in list of arduino's
-        arduinoGUI(self.mainframe)
-        arduinoGUI(self.mainframe)
+        # for element in list of arduino's (het is handig om dit mogelijk met een functie te doen zodat je die ook later kan aanroepen)
+        arduinoGUI(self.mainframe, 'arduino 1')
+        arduinoGUI(self.mainframe, 'arduino 2')
 
 
     def team():
