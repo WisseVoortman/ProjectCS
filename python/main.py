47a0ffd714bbc8a0a3a2023ffda610d215687d72
Mitchel <mitchelvanrijn@hotmail.nl>
2017-11-03 12:12:26 +0100

    Added features/bug fixes Reformatted some code Added a proper handler for closing the window Changed the credits function call Remade the credits function Added some code to the quit function to ensure the program fully closes

--- python/main.py
+++ python/main.py
@@ -1,12 +1,13 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 import serial.tools.list_ports
 import time
 from ArduinoGUI import *
 from PyCtrl import PyCtrl
 
 
-class mainView:
+class MainView:
     def __init__(self, master):
         """Initialize the main Program View.
         """
@@ -14,14 +15,16 @@ class mainView:
         self.master = master  # initialize Root window
         self.master.title('Centrale Project 2.1 - Computer System ')  # set title for Root window
         self.master.geometry("800x500+200+200")  # set size and location for Root window
-        self.mainframe = ttk.Notebook(self.master, padding="0 0 0 0")  # left top right bottem    create mainframe in Root winow
+        self.mainframe = ttk.Notebook(self.master,
+                                      padding="0 0 0 0")  # left top right bottem    create mainframe in Root winow
         self.mainframe.grid(column=0, row=0, sticky=(N, W, S, E))  # set mainframe to root windows size
+        self.master.protocol("WM_DELETE_WINDOW", lambda: self.quit())
 
         menubar = Menu(self.master)  # create a menubar
 
         # create a pulldown menu "FILE", and add it to the menu bar
         filemenu = Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Team", command=mainView.team)
+        filemenu.add_command(label="Credits", command=self.credits)
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=self.quit)
         menubar.add_cascade(label="Commands", menu=filemenu)  # se the label in the menubar
@@ -29,30 +32,33 @@ class mainView:
         # display the menu
         self.master.config(menu=menubar)
 
-        #label = Label(self.mainframe, text="Hieronder de control units: ", bg='red')
-        #label.pack()
-
-        # for element in list of arduino's (het is handig om dit mogelijk met een functie te doen zodat je die ook later kan aanroepen)
+        # List of all our views
         self.views = {}  # Empty dict
+
+        # The back-end process
         self.pyctrl = PyCtrl(self)
         self.pyctrl.start()
-        #ArduinoGUI(self.mainframe)
-        #ArduinoGUI(self.mainframe)
 
-
-    def team():
-        print("Team 6")
+    @staticmethod
+    def credits():
+        messagebox.showinfo("Credits - Team 6",
+                            "Dit geweldige software product is gemaakt door:\n"
+                            "Azad Ali\n"
+                            "Teun Ubels\n"
+                            "Mitchel van Rijn\n"
+                            "Wisse Voortman")
 
     def quit(self):
         self.pyctrl.stop()  # Will automatically stop all arduinos
         self.master.quit()
+        self.master.destroy()
+
+
 # END OF class: Mainview--------------------------------------------------------------------------
-
-
 def main():
     root = Tk()
-    mainView(root)
+    MainView(root)
     root.mainloop()
 
-main()
 
+main()
