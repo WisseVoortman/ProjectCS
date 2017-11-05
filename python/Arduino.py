c3c641a9e27d4d0135a54224b26c8378d2ba8d8c
Mitchel <mitchelvanrijn@hotmail.nl>
2017-11-05 22:27:32 +0100

    Added features/bug fixes Fixed a typo

--- python/Arduino.py
+++ python/Arduino.py
@@ -72,13 +72,13 @@ class Arduino:
                         voltage = analog_value * 5.0 / 1024
                         temperature = (voltage - 0.5) * 100
                         if DEBUG:
-                            print('[{0}]: Recevied {1} sensor reading - {2} - {3}v - {4} degrees C.'
+                            print('[{0}]: Received {1} sensor reading - {2} - {3}v - {4} degrees C.'
                                   .format(self._port, 'temperature', analog_value, voltage, temperature))
                     elif byte == COMMANDS.SEND_LIGHT:
                         reading = self._ser.readline()
                         analog_value = int(reading)
                         if DEBUG:
-                            print('[{0}]: Recevied {1} sensor reading - {2}'.format(self._port, 'light', analog_value))
+                            print('[{0}]: Received {1} sensor reading - {2}'.format(self._port, 'light', analog_value))
 
             except:
                 print(color('Failed to read data.', COLORS.RED))
